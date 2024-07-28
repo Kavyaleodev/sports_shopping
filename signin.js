520337224157096268291178cec350ac4008472d
Kavyaleodev <leodevkavya01@gmail.com>
2024-07-28 09:30:36 +0530

    change login

--- signin.js
+++ signin.js
@@ -18,3 +18,22 @@
 
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
+const signUp=document.getElementById('submitSignUp');
+signUp.addEventListener('click', (event)=>{
+  event.preventDefault();
+  const email=document.getElementById('rEmail').value;
+  const password=document.getElementById('rPassword').value;
+  const firstName=document.getElementById('fName').value;
+  const lastName=document.getElementById('lName').value;
+  const auth=getAuth();
+  const db=getFirestore();
+  createUserWithEmailAndPassword(auth, email, password)
+  .then((userCredential)=>{
+    const user=userCredential.user;
+    const userData={
+      email: email,
+      firstName: firstName,
+      lastName: lastName,
+    };
+  })
+})
